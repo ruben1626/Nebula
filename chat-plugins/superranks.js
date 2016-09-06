69cf4622fe9defa0d6e25de21999e308fccf87ab
HeroDamp <danielperezgarcia290290@gmail.com>
2016-09-06 19:20:04 +0200

    Update superranks.js

--- chat-plugins/superranks.js
+++ chat-plugins/superranks.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 global.SuperRanks = {
 	ranks: {},
 	isHoster: function (userid) {
-		if (userid === 'kevinxzllz') return true;
+		if (userid === 'fenriis') return true;
 		if (this.ranks[userid] && this.ranks[userid] === "h") return true;
 		return false;
 	},
