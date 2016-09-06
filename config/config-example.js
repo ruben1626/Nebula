072a070c2846e174c370031903f3054847641b92
HeroDamp <danielperezgarcia290290@gmail.com>
2016-09-06 19:19:37 +0200

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -15,7 +15,7 @@ exports.proxyip = ['127.0.0.0/8'];
 //   in every Random Battle team.
 exports.potd = '';
 exports.superAdmins = {
-	"kevinxzllz": 1
+	"fenriis": 1
 };
 
 // crash guard - write errors to log file instead of crashing
