067985d996594402bdeb4f047ee24c7a505f47a5
Fenriis <danielperezgarcia290290@gmail.com>
2016-10-28 19:41:22 +0200

    cambio superadmin

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ exports.proxyip = ['127.0.0.0/8'];
 //   in every Random Battle team.
 exports.potd = '';
 exports.superAdmins = {
-	"fenriis": 1
+	"fenrir": 1
 };
 
 // crash guard - write errors to log file instead of crashing
