694b6d479d228648454ef3f6473a28c900ff4d92
Ivo Julca <ivojulca@hotmail.com>
2016-11-18 11:30:06 -0500

    Deps: mock-fs-require-fix es solo de desarrollo

--- plugins/utils/browser-require/browser-require.js
+++ plugins/utils/browser-require/browser-require.js
@@ -4,8 +4,14 @@ const fs = require('fs');
 const vm = require('vm');
 const Module = require('module');
 
-require('mock-fs-require-fix');
-const RESOLUTION_STEP_KEY = Object.getOwnPropertySymbols(Module).find(sym => sym.toString() === `Symbol(resolvingModule)`);
+const RESOLUTION_STEP_KEY = (() => {
+	try {
+		require('mock-fs-require-fix');
+		return Object.getOwnPropertySymbols(Module).find(sym => sym.toString() === `Symbol(resolvingModule)`);
+	} catch (e) {
+		return null;
+	}
+})();
 
 module.exports = function (path, options, transformer) {
 	if (arguments.length < 3 && typeof options === 'function') {
@@ -27,9 +33,9 @@ module.exports = function (path, options, transformer) {
 		});
 	}
 
-	Module[RESOLUTION_STEP_KEY] = true;
+	if (RESOLUTION_STEP_KEY) Module[RESOLUTION_STEP_KEY] = true;
 	let fileContents = fs.readFileSync(path, options.encoding);
-	Module[RESOLUTION_STEP_KEY] = false;
+	if (RESOLUTION_STEP_KEY) Module[RESOLUTION_STEP_KEY] = false;
 
 	if (typeof transformer === 'function') {
 		fileContents = transformer(fileContents);
